3ee36df0ab92c7f6a4898453dd5a12ed6cb45fda
Lionel Sosa <lsosaestrada@my.apsu.edu>
2020-02-17 05:09:12 -0600

    Fixed bug where instructions for random tic tac toe wouldn't delete saved game.

--- app/src/main/java/com/joshuafoster/assignment1/RandomInstructionsActivity.java
+++ app/src/main/java/com/joshuafoster/assignment1/RandomInstructionsActivity.java
@@ -2,10 +2,18 @@ package com.joshuafoster.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 public class RandomInstructionsActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -21,6 +29,53 @@ public class RandomInstructionsActivity extends AppCompatActivity implements Vie
 
     @Override
     public void onClick(View v){
+        try {
+            // Exiting from main screen, deleting saved game
+            FileOutputStream fos = openFileOutput("savedGame.txt", Context.MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+            PrintWriter pw = new PrintWriter(bw);
+            pw.write("");
+            pw.close();
+        } catch (FileNotFoundException e) {
+            Log.e("File", "Couldn't open a file.");
+        }
+
+        try {
+            FileOutputStream fos2 = openFileOutput("random.txt", Context.MODE_PRIVATE);
+            OutputStreamWriter osw2 = new OutputStreamWriter(fos2);
+            BufferedWriter bw2 = new BufferedWriter(osw2);
+            PrintWriter pw2 = new PrintWriter(bw2);
+            pw2.write("no");
+            pw2.close();
+
+        } catch (FileNotFoundException e) {
+            Log.e("File", "Couldn't open a file.");
+        }
+
+        try {
+            FileOutputStream fos2 = openFileOutput("wild.txt", Context.MODE_PRIVATE);
+            OutputStreamWriter osw2 = new OutputStreamWriter(fos2);
+            BufferedWriter bw2 = new BufferedWriter(osw2);
+            PrintWriter pw2 = new PrintWriter(bw2);
+            pw2.write("no");
+            pw2.close();
+
+        } catch (FileNotFoundException e) {
+            Log.e("File", "Couldn't open a file.");
+        }
+
+        try {
+            FileOutputStream fos2 = openFileOutput("misere.txt", Context.MODE_PRIVATE);
+            OutputStreamWriter osw2 = new OutputStreamWriter(fos2);
+            BufferedWriter bw2 = new BufferedWriter(osw2);
+            PrintWriter pw2 = new PrintWriter(bw2);
+            pw2.write("no");
+            pw2.close();
+
+        } catch (FileNotFoundException e) {
+            Log.e("File", "Couldn't open a file.");
+        }
 
         if (v.getId() == R.id.PlayButton){
             Intent intent = new Intent(getApplicationContext(),
